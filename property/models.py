56246965cc727716c246afdf748d73dbf5fda616
949027 <82878668+949027@users.noreply.github.com>
2022-01-21 10:15:49 +0700

    Created relations 'owners-flats'

--- property/models.py
+++ property/models.py
@@ -11,7 +11,7 @@ class Owner(models.Model):
     flats = models.ManyToManyField(
         'Flat',
         related_name='owners',
-        verbose_name = 'Квартиры в собственности',
+        verbose_name='Квартиры в собственности',
     )
 
 
@@ -22,7 +22,7 @@ class Complaint(models.Model):
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
+    owner = models.CharField('ФИО владельца', max_length=200, db_index=True)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', blank=True)
     created_at = models.DateTimeField(
